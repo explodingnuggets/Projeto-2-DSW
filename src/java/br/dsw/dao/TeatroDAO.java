20d303f917506e95fd214ace084ce730f13d1f0c
Matheus Bortoleto <matheus.silva.ufscar@gmail.com>
2019-05-13 08:30:20 -0300

    Adiciona classe de entidade e DAO de teatro

---
+++ src/java/br/dsw/dao/TeatroDAO.java
@@ -0,0 +1,47 @@
+package br.dsw.dao;
+
+import br.dsw.pojo.Teatro;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class TeatroDAO extends GenericDAO<Teatro> {
+    @Override
+    public Teatro get(long id) {
+        EntityManager em = this.getEntityManager();
+        Teatro teatro = em.find(Teatro.class, id);
+        em.close();
+        
+        return teatro;
+    }
+    
+    @Override
+    public void save(Teatro teatro) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(teatro);
+        tx.commit();
+        em.close();
+    }
+    
+    @Override
+    public void update(Teatro teatro) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.merge(teatro);
+        tx.commit();
+        em.close();
+    }
+    
+    @Override
+    public void delete(Teatro teatro) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        teatro = em.getReference(Teatro.class, teatro.getId());
+        tx.begin();
+        em.remove(teatro);
+        tx.commit();
+        em.close();
+    }
+}
