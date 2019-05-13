592cc26cbb49b41383d93d375a17ca35227fed50
Matheus Bortoleto <matheus.silva.ufscar@gmail.com>
2019-05-13 08:58:21 -0300

    Adiciona unicidade entre promocao, site e teatro

--- src/java/br/dsw/pojo/Promocao.java
+++ src/java/br/dsw/pojo/Promocao.java
@@ -12,8 +12,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
+@Table(
+    uniqueConstraints={
+        @UniqueConstraint(columnNames={"id", "site"}),
+        @UniqueConstraint(columnNames={"id", "teatro"})
+    }
+)
 public class Promocao implements Serializable {
 
     @Id
