f33919ee7ebdee8d92a2b6b1758ae9788627845a
Matheus Bortoleto <matheus.silva.ufscar@gmail.com>
2019-05-13 08:51:12 -0300

    Adiciona classe de entidade e DAO para promocao

---
+++ src/java/br/dsw/pojo/Promocao.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.dsw.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Promocao implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private double preco;
+    private Date diaHorario;
+    
+    @ManyToOne
+    private SiteVendas site;
+    @ManyToOne
+    private Teatro teatro;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Date getDiaHorario() {
+        return diaHorario;
+    }
+
+    public void setDiaHorario(Date diaHorario) {
+        this.diaHorario = diaHorario;
+    }
+
+    public SiteVendas getSite() {
+        return site;
+    }
+
+    public void setSite(SiteVendas site) {
+        this.site = site;
+    }
+
+    public Teatro getTeatro() {
+        return teatro;
+    }
+
+    public void setTeatro(Teatro teatro) {
+        this.teatro = teatro;
+    }
+}
