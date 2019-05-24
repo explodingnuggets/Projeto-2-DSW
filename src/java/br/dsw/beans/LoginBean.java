8c5a3b48c704559d6a30bcf3e2d89f7ea73bbbb9
Rene Ferrante <rene.ferranten@gmail.com>
2019-05-24 01:28:27 -0300

    Tentative to render components just for logged in user

--- src/java/br/dsw/beans/LoginBean.java
+++ src/java/br/dsw/beans/LoginBean.java
@@ -2,15 +2,20 @@ package br.dsw.beans;
 
 import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
+import javax.faces.bean.SessionScoped;
 
 @ManagedBean
+@SessionScoped
 public class LoginBean implements Serializable {
 
-    public LoginBean() {
-      System.out.println("LoginBean started!");   
-   }
-    
+    private Boolean logged = false;
+
+    public Boolean getLogged() {
+        return logged;
+    }
+
     public String login() {
+        logged = true;
         return "/faces/index?faces-redirect=true";
     }
 }
